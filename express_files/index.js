ea3c744deed74d2bc0250ee587b87d93dacb662a
Akshay Maheshwari <akhem301>
2016-12-09 14:32:39 +0530

    added the authentication /signup

--- express_files/index.js
+++ express_files/index.js
@@ -46,46 +46,111 @@ app.get('/static_file_test',function(req,res){
     res.render('static_file_test');
 });
 
+app.get('/form', function(req,res){
+    res.render('form');
+})
+
 //-------------------------------forms------------------------------
 app.post('/', function(req, res){
     console.log(req.body);
     res.send("recieved your request!");
 });
-//--------------------------------------------------------------------
-//
-// app.get('/', function(req, res){
-//     res.cookie('name', 'express').send('cookie set'); //Sets name=express
-// });
-// var session= require('express-session');
-//
-// app.use(session({secret: "Shh, its a secret!"}));
-//
-// app.get('/', function(req, res){
-//    if(req.session.page_views){
-//       req.session.page_views++;
-//       res.send("You visited this page " + req.session.page_views + " times");
-//    }else{
-//       req.session.page_views = 1;
-//       res.send("Welcome to this page for the first time!");
-//    }
-// });
+//-------------------------------cookies-------------------------------------
 
+app.get('/cookie', function(req, res){
+    res.cookie('name', 'express').send('cookie set'); //Sets name=express
+});
 
-var things= require('./things.js');
+//----------------session---------------------------------------------
+var session= require('express-session');
 
-app.use('/things',things);
+app.use(session({secret: "Shh, its a secret!"}));
+
+app.get('/time', function(req, res){
+   if(req.session.page_views){
+      req.session.page_views++;
+      res.send("You visited this page " + req.session.page_views + " times");
+   }else{
+      req.session.page_views = 1;
+      res.send("Welcome to this page for the first time!");
+   }
+});
+//----------end-----------------------------------
+
+//-----------------authentication------------------------------
+var Users = [];
+
+app.get('/signup', function(req, res){
+    res.render('signup');
+});
+
+app.post('/signup', function(req, res){
+    if(!req.body.id || !req.body.password){
+        res.status("400");
+        res.send("Invalid details!");
+    }
+    else{
+        Users.filter(function(user){
+            if(user.id === req.body.id){
+                res.render('signup', {message: "User Already Exists! Login or choose another user id"});
+            }
+        });
+        var newUser = {id: req.body.id, password: req.body.password};
+        Users.push(newUser);
+        req.session.user = newUser;
+        res.redirect('/protected_page');
+    }
+});
+
+//-------------------checking login------------------------------------
+function checkSignIn(req, res ){
+    if(req.session.user){
+        next();     //If session exists, proceed to page
+    }
+    else {
+        var err = new Error("Not logged in!");
+    console.log(req.session.user);
+        next(err);  //Error, trying to access unauthorized page!
+    }
+}
+
+app.get('/protected_page', checkSignIn, function(req, res){
+    res.render('protected_page', {id: req.session.user.id})
+});
+
+app.get('/login', function(req, res){
+    res.render('login');
+});
+
+app.post('/login', function(req, res){
+    console.log(Users);
+    if(!req.body.id || !req.body.password){
+        res.render('login', {message: "Please enter both id and password"});
+    }
+    else{
+        Users.filter(function(user){
+            if(user.id === req.body.id && user.password === req.body.password){
+                req.session.user = user;
+                res.redirect('/protected_page');
+            }
+        });
+        res.render('login', {message: "Invalid credentials!"});
+    }
+});
+
+app.get('/logout', function(req, res){
+    req.session.destroy(function(){
+        console.log("user logged out.")
+    });
+    res.redirect('/login');
+});
+
+app.use('/protected_page', function(err, req, res, next){
+console.log(err);
+    //User should be authenticated! Redirect him to log in.
+    res.redirect('/login');
+});
+//////----------------------------------end---------------------------
+
 
 app.listen(3000);
-// app.get('/hello', function(req, res){
-// 	res.send("Hello World!");
-// });
-//
-// app.post('/hello', function(req, res){
-// 	res.send("You just called the post method at '/hello'!\n");
-// });
-//
-// app.all('/test', function(req, res){
-// 	res.send("HTTP method doesn't have any effect on this route!");
-// });
-//
-// app.listen(3000);
